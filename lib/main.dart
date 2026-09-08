f0fa190c5031b6c16a2651eb4bcc6a0dbb046134
Jaxon Svec <jaxonsvec@gmail.com>
2026-09-08 15:46:21 -0500

    inital button app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Does nothing
+            },
+            child: const Text('Button'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
